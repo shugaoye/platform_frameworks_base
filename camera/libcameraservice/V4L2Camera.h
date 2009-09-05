3f31cb97036da64e58fe20f7f99b4d6f2c2b0d2d
Yi Sun <beyounn@gmail.com>
2009-09-04 17:19:30 -0700

    add author's name

--- camera/libcameraservice/V4L2Camera.h
+++ camera/libcameraservice/V4L2Camera.h
@@ -3,6 +3,7 @@
  *      it under the terms of the GNU General Public License as published by
  *      the Free Software Foundation; either version 2 of the License, or
  *      (at your option) any later version.
+ *      Author: Niels Keeman  nielskeeman@gmail.com
  *
  */
 
